6adb99c4195fa497e64ddd78ea9cadeff5c044d3
antmedina <ant.n.medina@gmail.com>
2017-09-11 15:49:22 -0400

    Initial setup

---
+++ JS-Lab-6-091117.js
@@ -0,0 +1,11 @@
+
+document.addEventListener("DOMContentLoaded", function(){
+    for (var a = 0; a < 100; a++) {
+        var h3 = document.createElement("h3");
+        h3.innerHTML = "Accusation" + this.a + ":";
+        // h3.addEventListener('click', function () {
+            // let death 
+        // });
+        document.body.appendChild(h3);
+    }
+});
